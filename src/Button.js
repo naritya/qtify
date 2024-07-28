effabbac1416b4d2f3fbce508a58d674555b59e6
naritya <140882526+naritya@users.noreply.github.com>
2024-07-28 21:41:25 +0530

    h

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 const Button=()=>{
-    return(<button style={{"background-color": "black", "color":"green", borderRadius:"0.3rem", cursor:"pointer", marginRight:"5rem", width:"8rem", height:"2rem"}}>Give Feedback</button>);
+    return(<button style={{"background-color": "black", "color":"green", borderRadius:"0.3rem", cursor:"pointer", marginRight:"5rem", width:"8rem", height:"2rem", fontFamily:"Poppins"}}>Give Feedback</button>);
 }
  
 export default Button;
