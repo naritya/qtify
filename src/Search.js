055f464d3a70a7566ec07960379c8c01733b88be
naritya <140882526+naritya@users.noreply.github.com>
2024-07-29 01:03:19 +0530

    j

--- src/Search.js
+++ src/Search.js
@@ -4,13 +4,13 @@ import { ReactComponent as SearchIcon } from "./assets/search-icon.svg";
 
 const Search=()=>{
     return(
-    <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+    <form style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
         {/* <label htmlFor="search" className="search-label"></label> */}
-      <input  placeholder="Search a album of your choice" style={{width:"30rem", height:"2.5rem", margin:"0px", borderRadius:"0.4rem 0 0 0.4rem"}} />
-      <button style={{marginLeft:"0px", height:"2.85rem", cursor:"pointer", borderRadius:"0 0.4rem 0.4rem 0"}}>
+      <input  placeholder="Search a album of your choice" style={{ width:"30rem", height:"2.5rem", marginRight:"0px", borderRadius:"0.4rem 0 0 0.4rem"}} />
+      <button type="submit" style={{margin:"0px", height:"2.83rem", cursor:"pointer", borderRadius:"0 0.4rem 0.4rem 0", borderLeft:"none"}}>
         <SearchIcon />
       </button>
-    </div>);
+    </form>);
 }
 
 export default Search;
