d0590ffc7f6cb50f1ec37ec741a618c459dd56f2
naritya <140882526+naritya@users.noreply.github.com>
2024-07-28 18:26:42 +0530

    n

--- src/Search.js
+++ src/Search.js
@@ -3,10 +3,11 @@ import "./Search.css";
 import { ReactComponent as SearchIcon } from "./assets/search-icon.svg";
 
 const Search=()=>{
-    return(<div style={{display:"flex", justifyContent:"center"}}>
+    return(
+    <div style={{display:"flex", justifyContent:"center"}}>
         {/* <label htmlFor="search" className="search-label"></label> */}
       <input className="search" placeholder="Search a album of your choice" style={{width:"13rem"}} />
-      <button >
+      <button type="button">
         <SearchIcon />
       </button>
     </div>);
