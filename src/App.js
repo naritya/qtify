f66acf7a4b222847813635ac834da2c64f00c076
naritya <140882526+naritya@users.noreply.github.com>
2024-07-28 18:42:16 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./Navbar";
 import {Outlet} from "react-router-dom";
 import {fetchTopAlbums, fetchNewAlbums, fetchSongs, fetchFilters} from "./api.js"
+import  StyledEngineProvider  from "@mui/material/StyledEngineProvider";
 
 const App=()=>{
   const [data, setData]= useState({});
@@ -26,8 +27,10 @@ const {topAlbums=[], newAlbums=[], songs=[], genres=[]}= data;
 
   return(
   <>
+  <StyledEngineProvider injectFirst>
   <Navbar/>
  <Outlet context={{data: {topAlbums, newAlbums, songs}}}/>
+ </StyledEngineProvider>
   </>
 
   );
