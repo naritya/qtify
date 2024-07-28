6bb3f7dd28891a8ef522b1015950f60b7aa716fd
naritya <140882526+naritya@users.noreply.github.com>
2024-07-29 03:13:43 +0530

    h

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,14 +2,19 @@ import React from "react";
 import Button from "./Button";
 import Search from "./Search";
 import './Navbar.css';
-import LogoImage from "./assets/logoimage.png"
+import Logo from "./Logoqtify";
+import {Link} from "react-router-dom";
 
 
 export default function Navbar(){
     return(
         <>
             <nav className="navbar">
-                <div className="logoDiv"><img  src={LogoImage} alt="logo" width={80}/></div>
+                {/* <div className="logoDiv"><img  src={LogoImage} alt="logo" width={67}/></div> */}
+                <Link to="/">
+                <Logo />
+                </Link>
+                
                  <Search search={"Search a song of your choice"}/>
                 <Button children="Give Feedback"/>
             </nav>
