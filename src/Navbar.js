9022ab20e443a8eb29beb3127b7ee55bcce7c5de
naritya <140882526+naritya@users.noreply.github.com>
2024-07-30 23:15:34 +0530

    h

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,18 +6,14 @@ import Logo from "./Logoqtify";
 import {Link} from "react-router-dom";
 
 
-export default function Navbar(){
-    return(
-        <>
-            <nav className="navbar">
-                {/* <div className="logoDiv"><img  src={LogoImage} alt="logo" width={67}/></div> */}
-                <Link to="/">
-                <Logo />
-                </Link>
-                
-                 <Search search={"Search a song of your choice"}/>
-                <Button children="Give Feedback"/>
-            </nav>
-        </>
-    )
-}
+const Navbar = () => {
+    return (
+      <nav >
+        <Logo />
+        <Search />
+        <button className="nav-btn">Give Feedback</button>
+      </nav>
+    );
+  }
+
+  export default Navbar;
